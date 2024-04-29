07424064d3a7c71f508719553e5130fc8182dc17
Alexander Hecht <93292502+shushyu@users.noreply.github.com>
2024-04-29 23:21:56 +0200

    Create crfoid.sh
    create file

---
+++ crfoid.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+declare -A user_info  # Deklariere ein assoziatives Array
+
+# LDAP-Suche und Fülle das Array
+while IFS= read -r line; do
+    if [[ "$line" == mail:* ]]; then
+        email=$(echo "$line" | cut -d " " -f2)
+    elif [[ "$line" == displayName:* ]]; then
+        username=$(echo "$line" | cut -d " " -f2-)
+        user_info[$email]=$username
+    fi
+done < <(ldapsearch)
+
+for mail in "${!user_info[@]}"; do
+    
+    uid = $(echo "$mail" | cut -d "@" -f1)
+    echo "UID: $uid"
+    name = ${user_info[mail]}
+    echo "NAME: $name"
+    echo "MAIL: $mail"
+
+    # Überprüfe, ob sowohl E-Mail als auch Name vorhanden sind
+    # if [[ -n "$email" && -n "$name" ]]; then
+    #     mkdir -p "$uid"
+    #     identity_file="$uid/identities"
+    #     echo -n > "$identity_file" # Leere die Datei, falls sie bereits existiert
+    #     # Befülle die Datei mit dem JSON-Inhalt
+    #     echo "{\"---\":{" > "$identity_file"
+    #     echo " \"Id\":\"\",\"Label\":\"\",\"Email\":\"$email\",\"Name\":\"$name\",\"ReplyTo\":\"\",\"Bcc\":\"\",\"Signature\":\"\",\"SignatureInsertBefore\":false,\"SentFolder\":\"\",\"pgpEncrypt\":false,\"pgpSign\":false,\"smimeKey\":\"\",\"smimeCertificate\":\"\"" >> "$identity_file"
+    #     echo "}}" >> "$identity_file"
+    # else
+    #     echo "Eintrag für $username übersprungen, da Informationen fehlen."
+    # fi
+done
